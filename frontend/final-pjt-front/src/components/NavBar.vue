e3884423bca488e7579a53f62004c43c61b25c8e
mandu <alonemint@gmail.com>
2021-11-17 11:23:45 +0900

    [feat] initial code

---
+++ frontend/final-pjt-front/src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <router-link to="/">(로고)</router-link>
+    <router-link to="/login">(로그인)</router-link>
+    <router-link to="/profile">(프로필)</router-link>
+    <router-link to="/v404v">(404)</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+}
+</script>
+
+<style>
+
+</style>
