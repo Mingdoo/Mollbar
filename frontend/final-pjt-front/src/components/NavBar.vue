c98e38afa15c9ff3d4fb333447b06fab1312fd88
mandu <alonemint@gmail.com>
2021-11-18 01:16:39 +0900

    [feat] Update navbar skeleton

--- frontend/final-pjt-front/src/components/NavBar.vue
+++ frontend/final-pjt-front/src/components/NavBar.vue
@@ -1,10 +1,29 @@
 <template>
-  <div>
-    <router-link to="/">(로고)</router-link>
-    <router-link to="/login">(로그인)</router-link>
-    <router-link to="/profile">(프로필)</router-link>
-    <router-link to="/v404v">(404)</router-link>
-  </div>
+  <nav class="navbar navbar-light bg light">
+    <div class="container-fluid">
+      <router-link to="/" class="navbar-brand">(로고)</router-link>
+      <div class="navbar">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link to="/login" class="nav-link">(로그인)</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/signup" class="nav-link">(회원가입)</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/profile" class="nav-link">(프로필)</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/v404v" class="nav-link disabled">(404)</router-link>
+          </li>
+        </ul>
+      </div>
+    <form class="d-flex">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
+    </div>
+  </nav>
 </template>
 
 <script>
