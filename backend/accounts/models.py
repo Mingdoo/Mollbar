bd9cc6ae274c25fabce63f61eba7c217a1dd0036
by-gramm <ready2start@naver.com>
2021-11-17 20:05:10 +0900

    [feat] Create Rating model

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,5 +1,20 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.core.validators import (
+    MinValueValidator, MaxValueValidator
+)
+from movies.models import Movie
 
 
 class User(AbstractUser):
-    pass
+    ratings = models.ManyToManyField(Movie, through='Rating')
+
+
+class Rating(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user_movie_rate = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)])
+
+    def __str__(self):
+        return self.user_movie_rate
