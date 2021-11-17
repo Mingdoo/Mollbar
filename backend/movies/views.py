bae51a60960425051dab117a5f6fbf750900d1cc
by-gramm <ready2start@naver.com>
2021-11-17 17:46:34 +0900

    [feat] Add movie detail function

--- backend/movies/views.py
+++ backend/movies/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from .serializers import MovieSerializer
+from .models import Movie
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def movie_detail(request, movie_id):
+    # 영화 상세 정보
+    movie = get_object_or_404(Movie, movie_id=movie_id)
+    serializer = MovieSerializer(movie)
+    return Response(data=serializer.data, status=status.HTTP_200_OK)
