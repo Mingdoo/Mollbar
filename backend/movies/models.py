218b68c9c5e40fc19336604e9f7470cd68f57844
by-gramm <ready2start@naver.com>
2021-11-17 15:46:20 +0900

    [feat] Create GenreSerializer, MovieSerializer and Update Movie model

--- backend/movies/models.py
+++ backend/movies/models.py
@@ -17,7 +17,7 @@ class Movie(models.Model):
     vote_avg = models.DecimalField(max_digits=4, decimal_places=2)
     overview = models.TextField(null=True)
     poster_path = models.CharField(max_length=200, null=True)
-    genres = models.ManyToManyField(Genre, related_name='movies')
+    genres = models.ManyToManyField(Genre, related_name='movies', blank=True)
 
     def __str__(self):
         return self.title
---
+++ backend/movies/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Genre, Movie
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = '__all__'
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = '__all__'
