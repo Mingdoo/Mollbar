ceb1f5eada42e63daef0a18e394b24b5f87fb2b2
by-gramm <ready2start@naver.com>
2021-11-18 17:35:14 +0900

    [feat] Update Article model

--- backend/community/models.py
+++ backend/community/models.py
@@ -5,12 +5,10 @@ from movies.models import Movie
 
 class Article(models.Model):
     article_title = models.CharField(max_length=100)
-    is_review = models.BooleanField()
     article_content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    movie_id = models.ForeignKey(Movie, on_delete=models.CASCADE, blank=True, null=True)
 
     class Meta:
         ordering = ['-pk', ]
