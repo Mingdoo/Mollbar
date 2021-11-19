fe6428d26db2efccfe5ad1f1fb37479f9d3a5604
by-gramm <ready2start@naver.com>
2021-11-19 18:47:01 +0900

    [feat] Add rating CREATE/UPDATE/DELETE function

--- backend/movies/views.py
+++ backend/movies/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import get_list_or_404, get_object_or_404
-from .serializers import MovieSerializer
-from .models import Movie, Genre
-from django.db.models import F
+from django.shortcuts import get_object_or_404
+from .serializers import MovieSerializer, MovieDetailSerializer, RatingSerializer
+from .models import Movie, Rating
 
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
@@ -12,9 +11,11 @@ from rest_framework.response import Response
 @api_view(['GET'])
 @permission_classes([AllowAny])
 def movie_detail(request, movie_id):
-    # 영화 상세 정보
+    """
+    영화 상세 정보 및 해당 영화의 평점/리뷰 목록을 보여준다.
+    """
     movie = get_object_or_404(Movie, movie_id=movie_id)
-    serializer = MovieSerializer(movie)
+    serializer = MovieDetailSerializer(movie)
     return Response(data=serializer.data, status=status.HTTP_200_OK)
 
 
@@ -40,3 +41,38 @@ def get_popular_movies_by_genres(request, genre_id):
             {'message': '해당 장르를 찾을 수 없습니다.'}, 
             status=status.HTTP_404_NOT_FOUND
         )
+
+
+@api_view(['POST', 'DELETE'])
+def rate_movie(request, movie_id):
+    """
+    영화에 대한 평가를 등록/수정/삭제한다.
+    """
+    movie = get_object_or_404(Movie, movie_id=movie_id)
+    rating = movie.ratings.filter(user=request.user).first()
+
+    if request.method == 'POST':
+        # 해당 영화에 대한 평가가 없는 경우 => 평가 등록 (CREATE)
+        if not rating:
+            serializer = RatingSerializer(data=request.data)
+
+            if serializer.is_valid(raise_exception=True):
+                serializer.save(user=request.user, movie=movie)
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        # 해당 영화에 대한 평가가 있는 경우 => 평가 수정 (UPDATE)
+        else:
+            serializer = RatingSerializer(rating, data=request.data)
+
+            if serializer.is_valid(raise_exception=True):
+                serializer.save(user=request.user, movie=movie)
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    # 평가 삭제 (DELETE)
+    else:
+        if rating:
+            rating.delete()
+            return Response(
+                {'message': '평가가 삭제되었습니다.'},
+                status=status.HTTP_204_NO_CONTENT
+            )
