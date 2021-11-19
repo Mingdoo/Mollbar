0485b71d58eb88787c51f0984d361a502fa1d798
by-gramm <ready2start@naver.com>
2021-11-19 13:40:12 +0900

    [refactor] Move Rating model from accounts app to movies app

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import User, Rating
+from .models import User
 from django.contrib.auth.admin import UserAdmin
 
 
 admin.site.register(User, UserAdmin)
-admin.site.register(Rating)
