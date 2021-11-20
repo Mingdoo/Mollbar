6c642f007345b51e9505c5799e595deaf462a1ad
by-gramm <ready2start@naver.com>
2021-11-20 17:32:40 +0900

    [feat] Add password change function

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from django.contrib.auth import get_user_model
 
 
-class SignupSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
     
     class Meta:
