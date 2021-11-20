6a14b7f342fcb9cc5c6df23d2f20bce7aad547df
by-gramm <ready2start@naver.com>
2021-11-21 00:50:27 +0900

    [refactor] Update Movie model and movie_data.json

--- backend/movies/models.py
+++ backend/movies/models.py
@@ -22,6 +22,7 @@ class Movie(models.Model):
     overview = models.TextField(null=True)
     poster_path = models.CharField(max_length=200, null=True)
     genres = models.ManyToManyField(Genre, related_name='movies', blank=True)
+    video_url = models.CharField(max_length=20, null=True)
 
     class Meta:
         ordering = ['-popularity', ]
