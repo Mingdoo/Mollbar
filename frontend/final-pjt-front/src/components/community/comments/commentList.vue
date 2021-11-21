8e16310c47beb1e0475e7c5341a94d8cf052189a
mandu <alonemint@gmail.com>
2021-11-22 01:47:54 +0900

    [feat] add comment update

--- frontend/final-pjt-front/src/components/community/comments/commentList.vue
+++ frontend/final-pjt-front/src/components/community/comments/commentList.vue
@@ -1,21 +1,34 @@
 <template>
   <div>
     <h5 v-for="comment in comments" :key="comment.id">
-      {{ comment.content }}
-      {{ comment.comment_user }}
-      {{ comment.created_at }}
-      <button class="btn btn-warning" v-if="comment.comment_user === $store.state.userId" @click="deleteComment(comment)">Delete</button>
+      <div :id="`normalcomment${comment.id}`" class="commentformvisible">
+        {{ comment.content }}
+        <!-- {{ comment.comment_user }} -->
+        <!-- {{ comment.created_at }} -->
+        <button class="btn btn-primary" v-if="comment.comment_user === $store.state.userId" @click="updateComment(comment)">Edit</button>
+        <button class="btn btn-warning" v-if="comment.comment_user === $store.state.userId" @click="deleteComment(comment)">Delete</button>
+      </div>
+      <div :id="`editcomment${comment.id}`" class="commentformhidden">
+        <input type="text" name="" :value="comment.content" @input="userEditInput = $event.target.value">
+        <button class="btn btn-warning" @click="editComment(comment)">확인</button>
+        <button class="btn btn-primary" @click="cancelEditComment(comment)">취소</button>
+      </div>
     </h5>
   </div>
 </template>
 
 <script>
 const API_BASE_URL = 'http://127.0.0.1:8000/api/v1/community/'
-const token = localStorage.getItem('jwt')
+
 
 import axios from 'axios'
 export default {
   name: "CommentList",
+  data() {
+    return {
+      userEditInput: '',
+    }
+  },
   computed: {
     comments() {
       return this.$store.state.selectedArticleComments
@@ -26,6 +39,7 @@ export default {
   },
   methods: {
     deleteComment(comment) {
+      const token = localStorage.getItem('jwt')
       axios({
         method: 'delete',
         url: `${API_BASE_URL}${this.selectedArticle.id}/comments/${comment.id}/`,
@@ -37,11 +51,51 @@ export default {
             console.log(res)
           })
       // console.log(`${API_BASE_URL}${this.selectedArticle.id}/comments/${comment.id}/`)
+    },
+    updateComment(comment) {
+      const normalcomment = document.querySelector(`#normalcomment${comment.id}`)
+      normalcomment.classList.remove('commentformvisible')
+      normalcomment.classList.add('commentformhidden')
+
+      const editcomment = document.querySelector(`#editcomment${comment.id}`)
+      editcomment.classList.remove('commentformhidden')
+      editcomment.classList.add('commentformvisible')
+
+      // console.log(normalcomment)
+    },
+    editComment(comment) {
+      const token = localStorage.getItem('jwt')
+      axios({
+        method: 'put',
+        url: `${API_BASE_URL}${this.selectedArticle.id}/comments/${comment.id}/`,
+        headers: {
+          Authorization: `Bearer ${token}`
+        },
+        data: {
+          content: this.userEditInput
+        }
+      })
+        .then((res) => {
+          console.log(res)
+        })
+      const normalcomment = document.querySelector(`#normalcomment${comment.id}`)
+      normalcomment.classList.add('commentformvisible')
+      normalcomment.classList.remove('commentformhidden')
+
+      const editcomment = document.querySelector(`#editcomment${comment.id}`)
+      editcomment.classList.add('commentformhidden')
+      editcomment.classList.remove('commentformvisible')
     }
   }
 }
 </script>
 
 <style>
+.commentformvisible {
+  visibility: visible;
+}
 
+.commentformhidden {
+  display: none;
+}
 </style>
