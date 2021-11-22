bb6f8c90df9a6da808b7fa8e3f76463367efbe14
by-gramm <ready2start@naver.com>
2021-11-22 15:17:40 +0900

    [feat] Add wishlist load api

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('signup/', views.signup),
     path('password/', views.change_password),
     path('profile/<int:user_id>/', views.profile),
+    path('my_wishlist/', views.get_wishlist),
     # simplejwt
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
