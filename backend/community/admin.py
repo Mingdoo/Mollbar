9f8c76e7f7cc16d1b285fc35ed48e8e7ce230788
by-gramm <ready2start@naver.com>
2021-11-23 15:15:59 +0900

    [feat] create movie quiz app

--- backend/community/admin.py
+++ backend/community/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Article, Comment
+from .models import Article, Comment, Kmovie
 
 
 admin.site.register(Article)
 admin.site.register(Comment)
+admin.site.register(Kmovie)
